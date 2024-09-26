e5798adadfcdf6ef49e39504ef69ffe7fa51bb7a
Catalin Grumeza <catalin.grumeza@edu.itspiemonte.it>
2024-09-26 21:44:09 +0200

    Inizio CardUserService

---
+++ Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/servicies/UserCardsService.java
@@ -0,0 +1,20 @@
+package com.monkeysncode.servicies;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.monkeysncode.entites.UserCards;
+
+@Service
+public class UserCardsService {
+	@Autowired
+	UserCardDAO dao;
+	
+	
+	public List<UserCards>
+	
+	
+	
+}
