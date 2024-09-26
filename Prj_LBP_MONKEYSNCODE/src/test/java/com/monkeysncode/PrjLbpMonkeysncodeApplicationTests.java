1c862c9a40444d7effe81eb522884db87068acab
Catalin Grumeza <catalin.grumeza@edu.itspiemonte.it>
2024-09-26 15:09:29 +0200

    Init Project

---
+++ Prj_LBP_MONKEYSNCODE/src/test/java/com/monkeysncode/PrjLbpMonkeysncodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkeysncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjLbpMonkeysncodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
