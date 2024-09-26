5c704500f942a56e0d1f5ad591efb6dec3e0700c
AlessandraPedrotto <alessandra.pedrotto@edu.itspiemonte.it>
2024-09-26 21:06:03 +0200

    Correzione DeckDAO

--- Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/repos/DeckDAO.java
+++ Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/repos/DeckDAO.java
@@ -5,8 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.monkeysncode.entites.Deck;
+import com.monkeysncode.entites.User;
 
 public interface DeckDAO extends JpaRepository<Deck, Long>{
-	public List<Deck> findAll();
+	public List<Deck> findByUserId(User user);
 
 }
