2347fa82a5f05b43b0ac63032687fe585a733fc3
Catalin Grumeza <catalin.grumeza@edu.itspiemonte.it>
2024-10-04 22:45:42 +0200

    cards mapping fixed

--- Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/controllers/CardController.java
+++ Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/controllers/CardController.java
@@ -82,7 +82,7 @@ public class CardController {
 		 	param.put("desc", desc!= true ? "false" : "true");
 		 	model.addAttribute("param", param);
 
-	        return "cards";
+	        return "Cards";
 	        
 	 }
 	
