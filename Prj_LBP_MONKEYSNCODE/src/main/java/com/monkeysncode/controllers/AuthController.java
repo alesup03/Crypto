015506cfa49dcdaff1c7b393bd3810f7382024a4
Catalin Grumeza <catalin.grumeza@edu.itspiemonte.it>
2024-10-07 12:00:04 +0200

    fix to login and google confict when login and removed git auth

--- Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/controllers/AuthController.java
+++ Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/controllers/AuthController.java
@@ -14,23 +14,21 @@ import com.monkeysncode.servicies.UserService;
 public class AuthController {
 	@Autowired
 	UserService userService;
-
     @GetMapping("/login")
     public String loginPage() {
         return "login";  // Renderizza la pagina di login (con supporto per form e OAuth2)
     }
     
-
     @GetMapping("/register")
     public String registerPage(Model model) {
         model.addAttribute("user", new User());
         return "register";  // Renderizza la pagina di registrazione
     }
-
     @PostMapping("/register")
     public String registerUser(@ModelAttribute User user,Model model) {
-    	boolean exists=userService.exists(user);
-    	if(exists) {
+
+    	boolean userExists=userService.exists(user);
+    	if(userExists) {
     		model.addAttribute("exist", "Errore,Profilo esiste già");
     		return "register";
     	}
