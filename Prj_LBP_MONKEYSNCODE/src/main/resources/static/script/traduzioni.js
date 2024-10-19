41600a88dc5335008c0635092c6a7c9ac1e06e9a
AlessandraPedrotto <alessandra.pedrotto@edu.itspiemonte.it>
2024-10-19 16:17:49 +0200

    Aggiunti/cambiati commenti in inglese

--- Prj_LBP_MONKEYSNCODE/src/main/resources/static/script/traduzioni.js
+++ Prj_LBP_MONKEYSNCODE/src/main/resources/static/script/traduzioni.js
@@ -1,15 +1,15 @@
 function changeLanguage(lang) {
-  // Memorizza la lingua selezionata in localStorage
+  // Store the selected language in localStorage
   localStorage.setItem('selectedLanguage', lang);
 
-  // Carica il file JSON corrispondente alla lingua
+  // Load the corresponding JSON file for the selected language
   fetch(`../traduzioni/${lang}.json`)
     .then(response => response.json())
     .then(translations => {
-      // Trova tutti gli elementi con l'attributo data-translate
+      // Find all elements with the data-translate attribute
       const elementsToTranslate = document.querySelectorAll("[data-translate]");
 
-      // Sostituisci il testo di ogni elemento con la traduzione appropriata
+      // Replace the text of each element with the appropriate translation
       elementsToTranslate.forEach(element => {
         const key = element.getAttribute("data-translate");
         if (translations[key]) {
@@ -17,7 +17,7 @@ function changeLanguage(lang) {
         }
       });
 
-      // Cambia l'immagine dello slogan in base alla lingua selezionata
+      // Change the slogan image based on the selected language
       const sloganImage = document.getElementById("immagine-slogan");
       if (lang === 'it') {
         sloganImage.src = '/image/slogan-italiano.png';
@@ -39,7 +39,7 @@ function changeLanguage(lang) {
     .catch(error => console.error('Errore nel caricamento delle traduzioni:', error));
 }
 
-// Imposta la lingua predefinita (italiano) quando la pagina viene caricata
+// Set the default language (Italian) when the page loads
 document.addEventListener("DOMContentLoaded", function() {
   const savedLanguage = localStorage.getItem('selectedLanguage') || 'it'; // Italiano di default
   changeLanguage(savedLanguage);
