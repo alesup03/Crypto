611823a3ab259a49fe63f3dccdd3bec41e6d4583
alex <alexander.pelle@edu.itspiemonte.it>
2024-10-20 15:03:23 +0200

    Delete unused code

--- Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/repos/CardsDAO.java
+++ Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/repos/CardsDAO.java
@@ -1,13 +1,9 @@
 package com.monkeysncode.repos;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.monkeysncode.entites.Card;
 
--- Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/repos/UserImgDAO.java
+++ Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/repos/UserImgDAO.java
@@ -1,11 +1,9 @@
 package com.monkeysncode.repos;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.monkeysncode.entites.User;
 import com.monkeysncode.entites.UserImg;
 
 public interface UserImgDAO extends JpaRepository<UserImg, Long>{
