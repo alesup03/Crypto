8aee9d43fbab0c0200c1334da1164632949480cc
musty <mustafa.muhagr@edu.itspiemonte.it>
2024-11-07 22:46:47 +0100

    created Coin class

--- Prj_CryptoWallet/src/main/java/com/crypto_wallet/entities/Coin.java
+++ Prj_CryptoWallet/src/main/java/com/crypto_wallet/entities/Coin.java
@@ -1,9 +1,44 @@
 package com.crypto_wallet.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="coins") //il nome della table nel db
 public class Coin {
 
+	@Id
+	private int id;
+
+	private String name;
+	private String symbols;   //e.g BTC,ETH,....
+	private String img;       //e.g https://.png
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSymbols() {
+		return symbols;
+	}
+	public void setSymbols(String symbols) {
+		this.symbols = symbols;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+	
 }
-//id	coin_name	symbols	img
+
