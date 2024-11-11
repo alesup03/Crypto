bd8b80710168769cbdcfef0c553f331f5ceb34a0
musty <mustafa.muhagr@edu.itspiemonte.it>
2024-11-11 22:22:33 +0100

    creato UserDAO

--- Prj_CryptoWallet/src/main/java/com/crypto_wallet/services/UserServiceImp.java
+++ Prj_CryptoWallet/src/main/java/com/crypto_wallet/services/UserServiceImp.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.crypto_wallet.entities.User;
+import com.crypto_wallet.repos.UserDAO;
 @Service
 public class UserServiceImp implements UserService {
 
@@ -18,7 +19,7 @@ public class UserServiceImp implements UserService {
 	@Override
 	public void createNewUser(User user) {
 		// TODO Auto-generated method stub
-		boolean check = existByEmail(user.getEmail());
+		boolean check = dao.exitsByEmail(user.getEmail());
 		if(check) {
 			 new RuntimeException("email gia esiste");
 		}
